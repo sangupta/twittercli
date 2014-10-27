3ecfa586c98a1805043c5a54cadbcf0b5f64f872
Sandeep Gupta <sandy.pec@gmail.com>
2014-10-27 22:40:46 +0530

    Added mentions, retweets, retweets of me, and timeline commands

--- src/main/java/com/sangupta/twittercli/TwitterCLI.java
+++ src/main/java/com/sangupta/twittercli/TwitterCLI.java
@@ -30,6 +30,10 @@ import com.sangupta.jerry.store.PropertiesUserLocalStore;
 import com.sangupta.jerry.store.UserLocalStore;
 import com.sangupta.twittercli.command.Authorize;
 import com.sangupta.twittercli.command.Followers;
+import com.sangupta.twittercli.command.Mentions;
+import com.sangupta.twittercli.command.Retweets;
+import com.sangupta.twittercli.command.RetweetsOfMe;
+import com.sangupta.twittercli.command.Timeline;
 import com.sangupta.twittercli.command.Update;
 import com.sangupta.twittercli.command.Users;
 import com.sangupta.twittercli.command.Version;
@@ -48,7 +52,8 @@ public class TwitterCLI {
 										  .withDefaultCommand(Help.class)
 										  .withCommands(Help.class, Authorize.class, Version.class, 
 												  		WhoAmI.class, Followers.class, Update.class, 
-												  		WhoIs.class, Users.class);
+												  		WhoIs.class, Users.class, Retweets.class, 
+												  		RetweetsOfMe.class, Mentions.class, Timeline.class);
 		
 		
 		Cli<Runnable> cliParser = builder.build();
---
+++ src/main/java/com/sangupta/twittercli/command/Retweets.java
@@ -0,0 +1,38 @@
+package com.sangupta.twittercli.command;
+
+import io.airlift.command.Command;
+
+import com.google.gson.FieldNamingPolicy;
+import com.sangupta.jerry.http.WebInvoker;
+import com.sangupta.jerry.http.WebRequest;
+import com.sangupta.jerry.http.WebRequestMethod;
+import com.sangupta.jerry.http.WebResponse;
+import com.sangupta.jerry.util.AssertUtils;
+import com.sangupta.jerry.util.GsonUtils;
+import com.sangupta.twittercli.TwitterCommand;
+import com.sangupta.twittercli.domain.TwitterStatusUpdate;
+
+@Command(name = "retweets", description = "Show retweets of the current or given user")
+public class Retweets extends TwitterCommand {
+
+	@Override
+	public void doCommand() {
+		WebRequest request = WebInvoker.getWebRequest("https://api.twitter.com/1.1/statuses/retweets_of_me.json", WebRequestMethod.GET);
+		user.signRequest(request);
+		WebResponse response = WebInvoker.executeSilently(request);
+		TwitterStatusUpdate[] updates = GsonUtils.getGson(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).fromJson(response.getContent(), TwitterStatusUpdate[].class);
+		
+		if(AssertUtils.isEmpty(updates)) {
+			System.out.println("No retweets found");
+			return;
+		}
+		
+		for(TwitterStatusUpdate update : updates) {
+			System.out.println("  @" + update.user.getScreenName());
+			System.out.println("  " + update.text);
+			System.out.println("  " + update.createdAt);
+			System.out.println();
+		}
+	}
+
+}
